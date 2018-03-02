c2ca4d2faddd934069d5c93bc973d057a8adb0ae
Will Richards <richardsw2017@gmail.com>
2018-03-02 15:06:20 -0700

    working polyatail detection, added unit test(s), Dave would be proud

--- src/aluDetect.cpp
+++ src/aluDetect.cpp
@@ -1,8 +1,68 @@
+
 #include "fastqParse.h"
+#include <string.h>
+#include <list>
 #include <stdio.h>
 
+#include "aluDetect.h"
 
-int main(void){
+
+
+bool aluDetect::detectPolyATail(std::string seq) {
+  std::list<const char*> window;
+  int pos = 0;
+  float prop = 0.0;
+  if(seq.size() < 10 ){
+    return false;
+  }
+  else{
+    for(int i = 0; i < 10; ++i){
+      const char* c = &seq[i];
+      std::cout << "checking if init character " << *c << " equals " << 'A' << std::endl;
+      window.push_front(c);
+      if(*c=='A'){
+	prop+=0.1;
+	std::cout << *c << "should be A" << std::endl;
+	std::cout << "prop during initialization is: " << prop << std::endl;
+      }
+    }
+  }
+
+  if(prop >= 0.9){
+    std::cout << "Detected Poly a tail for sequence " << seq << " with score of " << prop << std::endl;
+    return true;
+  }
+
+  std::cout << "prop after initialization is: " << prop << std::endl;
+  
+  for(unsigned i = 10; i < seq.size(); ++i){
+    const char* c = &seq[i];
+    window.push_front(c);
+    
+
+    if(*c=='A' and *window.back() != 'A'){
+      prop+=1.0/(window.size()-1);
+      std::cout << "prop after addition is: " << prop << std::endl;
+      if(prop >= 0.9){
+	std::cout << "Detected Poly a tail for sequence " << seq << " with score of " << prop << std::endl;
+	return true;
+      }
+    }
+    else if(*c != 'A' and *window.back()=='A'){
+      prop-=1.0/(window.size()-1);
+      std::cout << "prop after subtraction is: " << prop << std::endl;
+    }
+    if(prop >= 0.9){
+      std::cout << "Detected Poly a tail for sequence " << seq << " with score of " << prop << std::endl;
+      return true;
+    }   
+    window.pop_back();
+    }
+  std::cout<< "Failed to detected Poly a tail for sequence "<< seq << " with score of " << prop << std::endl;
+  return false;
+}
+
+int my_main(void){
 
   std::string fastqPath = "../test_data/test.fastq";
   auto fastqSeqs = bioio::read_fastq_seqs(fastqPath);
@@ -10,36 +70,10 @@ int main(void){
 
   std::cout << fastqSeqs[0];
 
+  for(int i=0; i < fastqSeqs.size(); ++i){
+    aluDetect::detectPolyATail(fastqSeqs[i]);
+  }
 
   return 0;
-  
 }
 
-
-bool detectPolyATail(std::string seq) {
-  int pos = 0;
-  int prop = 0.0;
-  if(seq.size() < 10 ){
-    return false;
-  }
-  else{
-    char* window = new char[10]
-    for(int i = 0; i < 10; ++1){
-      char* c = seq[i];
-      if(strcmp(seq[i], 'A')==0){
-	prop+=0.1;
-      }
-      window[i] = c;
-    }
-  }
-  
-  for(unsigned i = 9; i < seq.size()-10; ++i){
-   
-  }
-  if(prop >= 0.9){
-    return true;
-  }
-  else{
-    return false;
-  } 
-}
