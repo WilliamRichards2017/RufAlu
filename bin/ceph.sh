d70d7596b4e3a3cdde5fbb958589c38496a27aed
Will Richards <richardsw2017@gmail.com>
2018-04-17 16:30:12 -0600

    removing bin scripts from .gitignore

---
+++ bin/ceph.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./../src/aluDetect /scratch/ucgd/lustre/u0691312/analysis/A414_CEPH/alu_samples/1348.bam.generator.V2.overlap.hashcount.fastq /scratch/ucgd/lustre/u0691312/analysis/A414_CEPH/alu_samples/1348.bam.generator.Mutations.fastq.bam /uufs/chpc.utah.edu/common/home/u0401321/RufAlu/test_data/primate_non-LTR_Retrotransposon.fasta /uufs/chpc.utah.edu/common/home/marth-ucgdstor/resources/references/human/GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa
---
+++ bin/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+./../src/aluDetect /uufs/chpc.utah.edu/common/home/u0401321/RUFUS/scripts/Family1.child.bam.generator.V2.overlap.hashcount.fastq /uufs/chpc.utah.edu/common/home/u0401321/RUFUS/scripts/Family1.child.bam.generator.Mutations.fastq.bam /uufs/chpc.utah.edu/common/home/u0401321/RufAlu/test_data/primate_non-LTR_Retrotransposon.fasta /uufs/chpc.utah.edu/common/home/marth-ucgdstor/resources/references/human/GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa
