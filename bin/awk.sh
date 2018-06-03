e977327dedc0d85c35b0c416eb4385984172d4ff
Will Richards <richardsw2017@gmail.com>
2018-06-03 17:46:19 -0600

    pushing before checkout

--- bin/awk.sh
+++ bin/awk.sh
@@ -3,7 +3,7 @@
 cat /uufs/chpc.utah.edu/common/home/u0401321/RufAlu/input/input.txt | while read line
 do
     f=$(echo $line | cut -d " " -f 3)
-    ./../src/aluDetect /scratch/ucgd/lustre/u0691312/analysis/A414_CEPH/alu_samples/"${f}".bam /scratch/ucgd/lustre/u0691312/analysis/A414_CEPH/alu_samples/"${f}".bam.generator.V2.overlap.hashcount.fastq  /uufs/chpc.utah.edu/common/home/u0401321/RufAlu/aluList/primate_non-LTR_Retrotransposon.fasta /uufs/chpc.utah.edu/common/home/marth-ucgdstor/resources/references/human/GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa &
+    ./../src/aluDetect /scratch/ucgd/lustre/u0991464/Projects/CEPH.25/"${f}".bam /scratch/ucgd/lustre/u0991464/Projects/CEPH.25/"${f}".bam.generator.V2.overlap.hashcount.fastq  /uufs/chpc.utah.edu/common/home/u0401321/RufAlu/aluList/primate_non-LTR_Retrotransposon.fasta /uufs/chpc.utah.edu/common/home/marth-ucgdstor/resources/references/human/GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa &
 
 done
  
